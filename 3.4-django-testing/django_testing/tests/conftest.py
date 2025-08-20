e8b99d5e7d5f5b678bd86bb759945c9e711fc6e7
Sergei <sergei-yudin@inbox.ru>
2025-08-20 18:33:02 +0300

    initial commit

---
+++ 3.4-django-testing/django_testing/tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from model_bakery import baker
+from rest_framework.test import APIClient
+
+from students.models import Course, Student
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+    return factory
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+    return factory
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,78 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_retrieve_course(api_client, course_factory):
+    course = course_factory()  # создаем курс
+
+    url = reverse('courses-detail', args=[course.id])
+    response = api_client.get(url)
+
+    assert response.status_code == 200
+    assert response.data['id'] == course.id
+    assert response.data['name'] == course.name
+
+@pytest.mark.django_db
+def test_list_courses(api_client, course_factory):
+    courses = course_factory(_quantity=10)
+
+    url = reverse('courses-list')
+    response = api_client.get(url)
+
+    assert response.status_code == 200
+    assert len(response.data) == len(courses)
+
+@pytest.mark.django_db
+def test_filter_courses_by_id(api_client, course_factory):
+    courses = course_factory(_quantity=5)
+    target_course = courses[2]
+
+    url = reverse('courses-list')
+    response = api_client.get(url, data={'id': target_course.id})
+
+    assert response.status_code == 200
+    assert len(response.data) == 1
+    assert response.data[0]['id'] == target_course.id
+
+@pytest.mark.django_db
+def test_filter_courses_by_name(api_client, course_factory):
+    courses = course_factory(_quantity=5)
+    target_course = courses[0]
+
+    url = reverse('courses-list')
+    response = api_client.get(url, data={'name': target_course.name})
+
+    assert response.status_code == 200
+    assert len(response.data) == 1
+    assert response.data[0]['name'] == target_course.name
+
+@pytest.mark.django_db
+def test_create_course(api_client):
+    url = reverse('courses-list')
+    data = {'name': 'Курс программирования'}
+    response = api_client.post(url, data = data, format='json')
+
+    assert response.status_code == 201
+    assert response.data['name'] == data['name']
+
+@pytest.mark.django_db
+def test_update_course(api_client, course_factory):
+    course = course_factory(name='Old name')
+    url = reverse('courses-detail', args=[course.id])
+
+    new_data = {'name': 'New name'}
+    response = api_client.patch(url, data=new_data, format='json')
+
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_course_delete(api_client, course_factory):
+    course = course_factory()
+    url = reverse('courses-detail', args=[course.id])
+
+    response = api_client.delete(url)
+
+    assert response.status_code == 204
+    from students.models import Course
+    assert not Course.objects.filter(id=course.id).exists()
